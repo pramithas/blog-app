bba91d20cb07ed8429037dbb14fea0cda7978182
Pramithas <dhakalpramithas@gmail.com>
2017-01-07 19:35:19 +0545

    Delete log file

---
+++ src/main/java/com/pramithas/Application.java
@@ -0,0 +1,19 @@
+package com.pramithas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import com.pramithas.configuration.SecurityConfiguration;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.pramithas.repository")
+@EntityScan(basePackages = "com.pramithas.models")
+@Import({ SecurityConfiguration.class })
+public class Application {
+	public static void main(String[] args) throws Throwable {
+		SpringApplication.run(Application.class, args);
+	}
+}
